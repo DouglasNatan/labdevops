109199bbae037addfd8fa8de3dff6c93ec33b2df
DouglasNatan <36136348+DouglasNatan@users.noreply.github.com>
2023-02-21 18:47:51 -0300

    changes 071

--- app.py
+++ app.py
@@ -2,15 +2,15 @@ from flask import Flask
 from flask_wtf.csrf import CSRFProtect
 
 def create_app():
-    app = Flask(__name__)
-    
+    app = Flask(__name__)    
     csrf = CSRFProtect(app)  
+    return app    
+
     
 @app.route("/")
 def pagina_inicial():
     return "Eu Amo Minha Família!S2...!"
 
-    return app    
     
 if __name__ == '__main__':
     app = create_app()
