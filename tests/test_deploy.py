4c1d7132134698a49a41000ce301b5e66fcf9cf0
DouglasNatan <36136348+DouglasNatan@users.noreply.github.com>
2023-02-21 15:24:57 -0300

    changes 033

--- tests/test_deploy.py
+++ tests/test_deploy.py
@@ -26,20 +26,19 @@ from labdevops import add
 import unittest
 
 class Test(unittest.TestCase):
-
-def test_deploy():
-    assert add(2, 2) == 4
-
-def setUp(self):
-    # cria uma instância do unittest, precisa do nome "setUp"
-    self.app = app.test_client()
-    # envia uma requisicao GET para a URL
-    self.result = self.app.get('/')
-
-def test_requisicao(self):
-    # compara o status da requisicao (precisa ser igual a 200)
-    self.assertEqual(self.result.status_code, 200)
+    def test_deploy():
+        assert add(2, 2) == 4
     
-def test_conteudo(self):
-    # verifica o retorno do conteudo da pagina
-    self.assertEqual(self.result.data.decode('utf-8'), "Eu Amo Minha Família!S2...!")
+    def setUp(self):
+        # cria uma instância do unittest, precisa do nome "setUp"
+        self.app = app.test_client()
+        # envia uma requisicao GET para a URL
+        self.result = self.app.get('/')
+    
+    def test_requisicao(self):
+        # compara o status da requisicao (precisa ser igual a 200)
+        self.assertEqual(self.result.status_code, 200)
+    
+    def test_conteudo(self):
+        # verifica o retorno do conteudo da pagina
+        self.assertEqual(self.result.data.decode('utf-8'), "Eu Amo Minha Família!S2...!")
