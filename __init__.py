5827d77f988a8fcc408cd2d419151abc60ee719d
DouglasNatan <36136348+DouglasNatan@users.noreply.github.com>
2023-02-21 16:08:40 -0300

    changes 040

--- __init__.py
+++ __init__.py
@@ -1,2 +1,10 @@
+from labdevops import app as flask_app
+
 def add(x, y):
     return x + y
+
+def app():
+    yield flask_app
+
+def client(app):
+    return app.test_client()
--- tests/test_deploy.py
+++ tests/test_deploy.py
@@ -26,22 +26,15 @@ import labdevops
 import unittest
 
 from labdevops import add
-from labdevops.app import app
+from labdevops import app
+from labdevops import client
 
 class Test(unittest.TestCase):
     def test_deploy():
         assert add(2, 2) == 4
     
-    def setUp(self):
-        # cria uma instância do unittest, precisa do nome "setUp"
-        self.app = app.test_client()
-        # envia uma requisicao GET para a URL
-        self.result = self.app.get('/')
-    
-    def test_requisicao(self):
-        # compara o status da requisicao (precisa ser igual a 200)
-        self.assertEqual(self.result.status_code, 200)
-    
-    def test_conteudo(self):
-        # verifica o retorno do conteudo da pagina
-        self.assertEqual(self.result.data.decode('utf-8'), "Eu Amo Minha Família!S2...!")
+    def test_app():
+        f = app.app()
+        c = app.client(f)
+        result = c.get('/')
+        assertEqual(result.data.decode('utf-8'), "Eu Amo Minha Família!S2...!")
