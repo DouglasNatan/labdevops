0c45ef1fd6a42db06995510b9dd10915d684b1f1
DouglasNatan <36136348+DouglasNatan@users.noreply.github.com>
2023-02-21 18:32:35 -0300

    changes 066

--- __init__.py
+++ __init__.py
@@ -1,21 +1,21 @@
-from flask import Flask
-from flask_wtf.csrf import CSRFProtect
-
-def create_app():
-    app = Flask(__name__)
-    
-    csrf = CSRFProtect(app)  
-    
-@app.route("/")
-def pagina_inicial():
-    return "Eu Amo Minha Família!S2...!"
-
-    return app    
-
-def add(x, y):
-    return x + y 
-    
-if __name__ == '__main__':
-    app = create_app()
-    app.run()
+#from flask import Flask
+#from flask_wtf.csrf import CSRFProtect
+#
+#def create_app():
+#    app = Flask(__name__)
+#    
+#    csrf = CSRFProtect(app)  
+#    
+#@app.route("/")
+#def pagina_inicial():
+#    return "Eu Amo Minha Família!S2...!"
+#
+#    return app    
+#
+#def add(x, y):
+#    return x + y 
+#    
+#if __name__ == '__main__':
+#    app = create_app()
+#    app.run()
 
