a45892a4bb2ca5654ea46e6dbd4f1e734cb0247f
DouglasNatan <36136348+DouglasNatan@users.noreply.github.com>
2023-04-02 15:10:27 -0300

    v1.0.6

--- test.py
+++ test.py
@@ -9,14 +9,15 @@ class Test(unittest.TestCase):
         self.app = app.test_client()
         # envia uma requisicao GET para a URL
         self.result = self.app.get('/')
+        self.result1 = self.app.get('/teste')
     def test_requisicao(self):
         # compara o status da requisicao (precisa ser igual a 200)
-        self.assertEqual(self.result.status_code, 200)
+        self.assertEqual(self.result1.status_code, 200)
     def test_conteudo(self):
         # verifica o retorno do conteudo da pagina
-        self.assertEqual(self.result.data.decode('utf-8'), "Fase 5 - Concluída!!!")
-    #def test_deploy():
-    #    assert add(2, 2) == 4
+        self.assertEqual(self.result1.data.decode('utf-8'), "Fase 5 - Concluída!!!")
+    def test_request1():
+        self.assertEqual(self.result.status_code, 200)
 
 if __name__ == "__main__":
         unittest.main()
